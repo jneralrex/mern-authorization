f1d50c92464bb429805072232644e9ee1d01793c
Idah Abubakar Isah <idahabubakar49@gmail.com>
2024-09-05 16:54:08 +0100

    added pages and created backed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,28 @@
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Profile from './pages/Profile'
+import About from './pages/About'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route index element={<SignUp/>}/>
+      <Route path='signin' element={<SignIn/>}/>
+      <Route path='home' element={<Home/>}>
+      <Route path='profile' element={<Profile/>}/>
+      <Route path='about' element={<About/>}/>
+      </Route>
+    </Route>
+  )
+)
 
 function App() {
 
   return (
     <>
-    <h1 className="">App</h1>
+    <RouterProvider router={router}/>
     </>
   )
 }
