453bf854e50824838e58327a6ae8ee1130c14a69
Idah Abubakar Isah <idahabubakar49@gmail.com>
2024-12-03 12:57:29 +0100

    correcting deployment issues

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Static Files Middleware
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.send(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 // Routes
