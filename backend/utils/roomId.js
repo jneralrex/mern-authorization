8760f5317a25916c50d91623f3da7efd8d6a52e8
Idah Abubakar Isah <idahabubakar49@gmail.com>
2024-12-24 20:49:49 +0100

    corrected module export

--- backend/utils/roomId.js
+++ backend/utils/roomId.js
@@ -1,6 +1,6 @@
- const generateRoomId = (id1, id2) => {
-    return [id1, id2].sort().join("_");
-  };  
+// Change CommonJS export to ES Module export
+const generateRoomId = (id1, id2) => {
+  return [id1, id2].sort().join("_");
+};
 
-  module.exports = generateRoomId;
-  
+export default generateRoomId;
