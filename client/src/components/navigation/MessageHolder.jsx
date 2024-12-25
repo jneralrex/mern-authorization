ae0908c23fa7565841559769b64e01b96ce47616
Idah Abubakar Isah <idahabubakar49@gmail.com>
2024-12-25 20:14:39 +0100

    Sorted socket error

--- client/src/components/navigation/MessageHolder.jsx
+++ client/src/components/navigation/MessageHolder.jsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 import { FaCamera, FaMicrophone, FaSmile, FaPhoneAlt, FaTimes } from "react-icons/fa";
 import { useSelector } from "react-redux";
